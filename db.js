4f1a42ea5a139a2128dc4cb664277b058a91327a
Ankur10gen <rainaankur92@gmail.com>
2023-04-09 01:08:45 +0800

    introduced new commands and find many and search on db

--- db.js
+++ db.js
@@ -11,7 +11,8 @@ var config = {
 };
             
 function writeToDB(data) {
-    axios({config, data: data})
+    config['data'] = data
+    axios(config)
     .then(function (response) {
         console.log(JSON.stringify(response.data));
     })
@@ -20,4 +21,47 @@ function writeToDB(data) {
     });
 }
 
-module.exports = {writeToDB}
+async function findFromDB(data){
+    const customPromise = new Promise((resolve, reject) => {
+    config['data'] = data
+    config['url'] = 'https://ap-southeast-1.aws.data.mongodb-api.com/app/data-nvdxf/endpoint/data/v1/action/find'
+    let res = ""
+    axios(config)
+    .then(function (response) {
+        console.log("I was here")
+        console.log(JSON.stringify(response.data));
+        res = JSON.stringify(response.data)
+        resolve(res)
+    })
+    .catch(function (error) {
+        console.log("came here")
+        console.log(error);
+        reject(res)
+    });    
+    })
+    return customPromise
+}
+
+async function searchDB(searchString){
+    const customPromise = new Promise((resolve, reject) => {
+    let cfg = {
+        method:'post',
+        url: 'https://us-east-1.aws.data.mongodb-api.com/app/slack-backend-pzncy/endpoint/searchReadingLists?searchString='+searchString,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    } 
+    axios(cfg)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        resolve(JSON.stringify(response.data))
+    })
+    .catch(function (error) {
+        console.log(error);
+        reject("")
+    });
+})
+return customPromise
+}
+
+module.exports = {writeToDB, findFromDB, searchDB}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { App } = require('@slack/bolt');
-const { writeToDB } = require('./db')
+const { writeToDB, findFromDB, searchDB } = require('./db')
+require('dotenv').config()
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -22,34 +23,40 @@ app.event('app_home_opened', async ({ event, client, logger }) => {
             "type": "home",
             "blocks": [
                 {
-                    "type": "header",
-                    "text": {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "*What would you like your colleagues to read?*"
+                        }
+                    ]
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "link_input"
+                    },
+                    "label": {
                         "type": "plain_text",
-                        "text": "New request",
+                        "text": "Link",
                         "emoji": true
-                    }
+                    },
+                    "block_id":"link_input"
                 },
                 {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": "*Type:*\nPaid Time Off"
-                        },
-                        {
-                            "type": "mrkdwn",
-                            "text": "*Created by:*\n<example.com|Fred Enriquez>"
-                        }
-                    ]
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": "*When:*\nAug 10 - Aug 13"
-                        }
-                    ]
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "multiline": true,
+                        "action_id": "summary_input"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Summary",
+                        "emoji": true
+                    },
+                    "block_id":"summary_input"
                 },
                 {
                     "type": "actions",
@@ -58,22 +65,11 @@ app.event('app_home_opened', async ({ event, client, logger }) => {
                             "type": "button",
                             "text": {
                                 "type": "plain_text",
-                                "emoji": true,
-                                "text": "Approve"
+                                "text": "Submit",
+                                "emoji": true
                             },
-                            "style": "primary",
                             "value": "click_me_123",
                             "action_id": "actionId-0"
-                        },
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "emoji": true,
-                                "text": "Reject"
-                            },
-                            "style": "danger",
-                            "value": "click_me_123"
                         }
                     ]
                 }
@@ -82,6 +78,7 @@ app.event('app_home_opened', async ({ event, client, logger }) => {
       });
   
       logger.info(result);
+      //console.log(await client.users.info({user:event.user}))
     }
     catch (error) {
       logger.error(error);
@@ -90,23 +87,31 @@ app.event('app_home_opened', async ({ event, client, logger }) => {
 
 // Your listener function will be called every time an interactive component with the action_id "approve_button" is triggered
 app.action('actionId-0', async ({ body, client, ack }) => {
-    console.log("Button was pressed")
     await ack();
-    
+    console.log("Button was pressed")
+    console.log(body.view.id, JSON.stringify(body.view.state))
+
+    const link = body.view.state['values']['link_input']['link_input']['value']
+    const summary = body.view.state['values']['summary_input']['summary_input']['value']
+    const username = body.user.username
+
     var data = JSON.stringify({
         "collection": "activity",
         "database": "slack",
         "dataSource": "all-hands-dev",
         "document": {
-            "status": "open",
-            "text": "Do the dishes"
+            "link": link,
+            "summary": summary,
+            "username": username,
+            "createdAt":{ "$date": new Date(Date.now()).toISOString() }
           }
-    });
+    })
 
-    await writeToDB(data)
-    const result = await client.views.publish({
+    writeToDB(data);
+    
+     const result = await client.views.update({
         // Use the user ID associated with the event
-        user_id: body.user.id,
+        view_id:body.view.id,
         view: {
             "type": "home",
             "blocks": [
@@ -114,11 +119,111 @@ app.action('actionId-0', async ({ body, client, ack }) => {
                     "type": "header",
                     "text": {
                         "type": "plain_text",
-                        "text": "New request",
+                        "text": "Submission Successful.",
                         "emoji": true
                     }
                 }]}});
-    // Update the message to reflect the action
+
+});
+
+app.command('/readinglist', async ({ command, ack, respond }) => {
+    // Acknowledge command request
+    await ack();
+  
+    //await respond(JSON.stringify(command.text));
+
+    var data = {
+        "collection": "activity",
+        "database": "slack",
+        "dataSource": "all-hands-dev"
+    }
+
+    let text = command.text
+
+    console.log("text: ", text)
+
+    if(text == ""){
+        text = command.user_name
+    }
+
+    if(text.charAt(0) == "@") {
+        text = text.slice(1,)
+    }
+
+    let beautifulMsgs = {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*These articles may be relevant to* _" + text + "_ ."
+                }
+            }
+        ]
+    }
+
+    data["filter"] = {"username": text}
+    const res = await findFromDB(JSON.stringify(data))
+    const parseRes = JSON.parse(res)["documents"]
+    console.log(parseRes[0], parseRes.length)
+    try{
+        for(var i=0;i<20;i++){
+            try {
+            let bmsg = {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": i+1+". *"+parseRes[i]['summary']+"* <"+parseRes[i]['link']+"|"+"Read More Here>"
+                }
+            }
+            console.log(bmsg)
+            beautifulMsgs.blocks.push(bmsg)
+        } catch(err) {
+            console.log(err, parseRes[i])
+        }
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
+    respond(beautifulMsgs)
+  });
+
+  // Search on Reading List
+  app.command('/readinglistsearch', async ({ command, ack, respond }) => {
+    // Acknowledge command request
+    await ack();
+  
+    let text = command.text
+    const res = await searchDB(JSON.stringify(text))
+    const parseRes = JSON.parse(res)
+    let beautifulMsgs = {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*These articles may be relevant to* _" + text + "_ ."
+                }
+            }
+        ]
+    }
+    for(var i=0;i<parseRes.length;i++){
+        try {
+        let bmsg = {
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": i+1+". *"+parseRes[i]['summary']+"* <"+parseRes[i]['link']+"|"+"Read More Here>"
+			}
+		}
+        beautifulMsgs.blocks.push(bmsg)
+    } catch(err) {
+        console.log(err)
+    }
+    }
+    console.log(beautifulMsgs)
+    respond(beautifulMsgs)
   });
 
 (async () => {
